24a91ecced6ef80ca171f6ff5bd95cd511c1b9d5
currybab <pj2417@gmail.com>
2019-01-05 19:38:32 +0900

    tota result server will be changed to pure node js function

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { Api, JsonRpc, RpcError } from 'eosjs';
+import JsSignature from 'eosjs/dist/eosjs-jssig';
+import env from './env';
+
+
+const startTime = 1544025600000;
+const intervalTime = 300000;
+
+
+
